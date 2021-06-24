3256a761c4bd283aef2d5639a8846bb16f293a44
xsoroton <xsoroton@gmail.com>
2021-06-24 19:59:20 +1000

    Init commit, fixed name space to local

---
+++ racing/go.mod
@@ -0,0 +1,17 @@
+module github.com/xsoroton/entain/racing
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.3.0
+	github.com/mattn/go-sqlite3 v1.14.6
+	github.com/sirupsen/logrus v1.8.0
+	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+	google.golang.org/genproto v0.0.0-20210226172003-ab064af71705
+	google.golang.org/grpc v1.36.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0
+	google.golang.org/protobuf v1.26.0
+	syreclabs.com/go/faker v1.2.3
+)
---
+++ racing/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"net"
+
+	"database/sql"
+
+	"google.golang.org/grpc"
+	log "github.com/sirupsen/logrus"
+
+
+	"github.com/xsoroton/entain/racing/db"
+	"github.com/xsoroton/entain/racing/proto/racing"
+	"github.com/xsoroton/entain/racing/service"
+)
+
+var (
+	grpcEndpoint = flag.String("grpc-endpoint", "localhost:9000", "gRPC server endpoint")
+)
+
+func main() {
+	flag.Parse()
+
+	if err := run(); err != nil {
+		log.Fatalf("failed running grpc server: %s", err)
+	}
+}
+
+func run() error {
+	conn, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		return err
+	}
+
+	racingDB, err := sql.Open("sqlite3", "./db/racing.db")
+	if err != nil {
+		return err
+	}
+
+	racesRepo := db.NewRacesRepo(racingDB)
+	if err := racesRepo.Init(); err != nil {
+		return err
+	}
+
+	grpcServer := grpc.NewServer()
+
+	racing.RegisterRacingServer(
+		grpcServer,
+		service.NewRacingService(
+			racesRepo,
+		),
+	)
+
+	log.Infof("gRPC server listening on: %s", *grpcEndpoint)
+
+	if err := grpcServer.Serve(conn); err != nil {
+		return err
+	}
+
+	return nil
+}
